f7a096c259e30034ed7344a3a41ac294174ad49b
suhail <suhailsulfekar@gmail.com>
2020-05-30 14:39:32 +0530

    change the sketch file

--- sketch.js
+++ sketch.js
@@ -18,8 +18,8 @@ function draw() {
   && fixedRect.x - movingRect.x < fixedRect.width/2+movingRect.width/2 
   && fixedRect.y - movingRect.y < fixedRect.height/2+movingRect.height/2 
   &&  movingRect.y - fixedRect.y < fixedRect.height/2+movingRect.height/2 ){
-    fixedRect.shapeColor = "red";
-     movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "blue";
+     movingRect.shapeColor = "blue";
   }
   else{
     fixedRect.shapeColor = "green";
